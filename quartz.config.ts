def8ec717192717886b2e6296d5856f1c52dc6c9
Molluscholar <252650609+Molluscholar@users.noreply.github.com>
2026-04-22 16:03:56 +0900

    修正

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "ja-JP",
     baseUrl: "molluscholar.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
