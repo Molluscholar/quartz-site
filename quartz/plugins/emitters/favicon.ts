919d9a12cabd690c0e8abbf362cdc322126415db
Molluscholar <252650609+Molluscholar@users.noreply.github.com>
2026-04-23 00:49:02 +0900

    デザイン修正

--- quartz/plugins/emitters/favicon.ts
+++ quartz/plugins/emitters/favicon.ts
@@ -1,5 +1,6 @@
 import sharp from "sharp"
-import { joinSegments, QUARTZ, FullSlug } from "../../util/path"
+import { resolve } from "node:path"
+import { FullSlug } from "../../util/path"
 import { QuartzEmitterPlugin } from "../types"
 import { write } from "./helpers"
 import { BuildCtx } from "../../util/ctx"
@@ -7,8 +8,7 @@ import { BuildCtx } from "../../util/ctx"
 export const Favicon: QuartzEmitterPlugin = () => ({
   name: "Favicon",
   async *emit({ argv }) {
-    const iconPath = joinSegments(QUARTZ, "static", "icon.png")
-
+    const iconPath = resolve(process.cwd(), "quartz", "static", "iconmoll.png")
     const faviconContent = sharp(iconPath).resize(48, 48).toFormat("png")
 
     yield write({
